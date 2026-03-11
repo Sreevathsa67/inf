cc69acedde94908fadcadd26981bb4021cd29f84
Sreevathsa67 <sreevathsarnms6@gmail.com>
2026-03-11 14:47:17 +0530

    redeploy

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const Register = () => {
   useEffect(() => {
-    window.location.href = "https://unstop.com/p/infothon-60-vidyavardhaka-college-of-engineering-mysore-1656421";
+    window.location.replace("https://unstop.com/p/infothon-60-vidyavardhaka-college-of-engineering-mysore-1656421");
   }, []);
   return null;
 };
