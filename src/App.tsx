cb58e3ed3c639845e87cf16bf662f1a251c2b3ab
Sreevathsa67 <sreevathsarnms6@gmail.com>
2026-03-11 13:09:16 +0530

    done2222222222222

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import BackgroundEffects from "@/components/BackgroundEffects";
-import Index from "./pages/Index";
-import Problems from "./pages/Problems";
-import Sponsors from "./pages/Sponsors";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Register from "./pages/Register";
-import NotFound from "./pages/NotFound";
+
+const Index    = lazy(() => import("./pages/Index"));
+const Problems = lazy(() => import("./pages/Problems"));
+const Sponsors = lazy(() => import("./pages/Sponsors"));
+const About    = lazy(() => import("./pages/About"));
+const Contact  = lazy(() => import("./pages/Contact"));
+const Register = lazy(() => import("./pages/Register"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 const queryClient = new QueryClient();
 
@@ -23,15 +25,17 @@ const App = () => (
       <BrowserRouter>
         <BackgroundEffects />
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/problems" element={<Problems />} />
-          <Route path="/sponsors" element={<Sponsors />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/"         element={<Index />} />
+            <Route path="/problems" element={<Problems />} />
+            <Route path="/sponsors" element={<Sponsors />} />
+            <Route path="/about"    element={<About />} />
+            <Route path="/contact"  element={<Contact />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*"         element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
